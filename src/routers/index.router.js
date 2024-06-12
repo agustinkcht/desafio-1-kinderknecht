c0c90cefdb66a27e35e1f46b7868d8d98c33ba57
agustinkcht <agustinkcht@gmail.com>
2024-06-12 14:35:39 -0300

    environment configurations, introducing global&child processes

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,13 +1,24 @@
 import CustomRouter from "./CustomRouter.js";
-import apiRouter from './api/index.api.js'
-
+import apiRouter from "./api/index.api.js";
+import { fork } from "child_process";
 
 class IndexRouter extends CustomRouter {
-    init() {
-        this.use('/api', apiRouter)
-    }
-}
+  init() {
+    this.use("/api", apiRouter);
+    this.read("/fork", ["PUBLIC"], (req, res, next) => {
+      try {
+        const childProcess = fork("./src/processes/sum.proc.js");
+        childProcess.send("start");
+        childProcess.on("message", (result) => {
+          return res.suc200res(result);
+        });
+      } catch (err) {
+        return next(err);
+      }
+    });
+  };
+};
 
-const indexRouter = new IndexRouter()
+const indexRouter = new IndexRouter();
 
-export default indexRouter.getRouter()
+export default indexRouter.getRouter();
---
+++ src/utils/args.util.js
@@ -0,0 +1,16 @@
+import { Command } from "commander";
+
+const args = new Command();
+
+args.option("-p <port>", "port", 8080);
+args.option("--env <env>", "environment", "prod");
+args.option("--persistence <persistence>", "persistence", "mongo");
+// 1. flag, 2. description, 3. default
+
+args.parse();
+
+export default args.opts();
+
+
+
+
